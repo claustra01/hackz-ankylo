ce8a19c0d8f25e3ac73b9d64a7a81b8355e3891c
claustra01 <shinya430.g@gmail.com>
2024-12-15 02:18:48 +0900

    add: navigate

--- frontend/src/pages/title.tsx
+++ frontend/src/pages/title.tsx
@@ -1,31 +1,59 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const TitleStyle = {
+	fontSize: "6rem",
+	margin: "1rem",
+};
+
+const TextInputStyle = {
+	width: "24rem",
+	height: "4rem",
+	color: "black",
+	border: "1px solid gray",
+	borderRadius: "8px",
+	boxShadow: "0px 5px 15px 0px rgba(0, 0, 0, 0.35)",
+	fontSize: "1.5rem",
+	margin: "3rem",
+	padding: "1rem",
+};
+
+const buttonStyle = {
+	width: "16rem",
+	height: "4rem",
+	color: "green",
+	backgroundColor: "white",
+	border: "1px solid green",
+	borderRadius: "8px",
+	boxShadow: "0px 5px 15px 0px rgba(0, 0, 0, 0.35)",
+	fontSize: "1.5rem",
+	margin: "1rem",
+};
+
 const Title = () => {
-	const TitleStyle = {
-		fontSize: "6rem",
-		margin: "1rem",
+	const navigate = useNavigate();
+	const [roomPassword, setRoomPassword] = useState<string>("");
+
+	const handleRoomPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setRoomPassword(e.target.value);
 	};
 
-	const TextInputStyle = {
-		width: "24rem",
-		height: "4rem",
-		color: "black",
-		border: "1px solid gray",
-		borderRadius: "8px",
-		boxShadow: "0px 5px 15px 0px rgba(0, 0, 0, 0.35)",
-		fontSize: "1.5rem",
-		margin: "3rem",
-		padding: "1rem",
+	const handleRedirectToVR = () => {
+		if (roomPassword === "") {
+			alert("あいことばを入力してください");
+			return;
+		}
+		const hash = roomPassword;
+		navigate(`/vr#${hash}`);
 	};
 
-	const buttonStyle = {
-		width: "16rem",
-		height: "4rem",
-		color: "green",
-		backgroundColor: "white",
-		border: "1px solid green",
-		borderRadius: "8px",
-		boxShadow: "0px 5px 15px 0px rgba(0, 0, 0, 0.35)",
-		fontSize: "1.5rem",
-		margin: "1rem",
+	const handleRedirectToPC = () => {
+		if (roomPassword === "") {
+			alert("あいことばを入力してください");
+			return;
+		}
+		const hash = roomPassword;
+		navigate(`/vr#${hash}`);
 	};
 
 	return (
@@ -38,11 +66,16 @@ const Title = () => {
 			}}
 		>
 			<h1 style={TitleStyle}>弓DOOOOON</h1>
-			<input type="text" placeholder="あいことば" style={TextInputStyle} />
-			<button type="button" style={buttonStyle}>
+			<input
+				type="text"
+				placeholder="あいことば"
+				style={TextInputStyle}
+				onChange={handleRoomPasswordChange}
+			/>
+			<button type="button" style={buttonStyle} onClick={handleRedirectToVR}>
 				VRでプレイ
 			</button>
-			<button type="button" style={buttonStyle}>
+			<button type="button" style={buttonStyle} onClick={handleRedirectToPC}>
 				PCでプレイ
 			</button>
 		</div>
