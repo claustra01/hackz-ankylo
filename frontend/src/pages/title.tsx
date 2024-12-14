366f2869fc9cb8732ea97b03ca6a78335b0c7ec9
claustra01 <shinya430.g@gmail.com>
2024-12-15 02:02:55 +0900

    add: ui shadow

--- frontend/src/pages/title.tsx
+++ frontend/src/pages/title.tsx
@@ -1,6 +1,7 @@
 const Title = () => {
 	const TitleStyle = {
-		fontSize: "4rem",
+		fontSize: "6rem",
+		margin: "1rem",
 	};
 
 	const TextInputStyle = {
@@ -9,6 +10,7 @@ const Title = () => {
 		color: "black",
 		border: "1px solid gray",
 		borderRadius: "8px",
+		boxShadow: "0px 5px 15px 0px rgba(0, 0, 0, 0.35)",
 		fontSize: "1.5rem",
 		margin: "3rem",
 		padding: "1rem",
@@ -21,6 +23,7 @@ const Title = () => {
 		backgroundColor: "white",
 		border: "1px solid green",
 		borderRadius: "8px",
+		boxShadow: "0px 5px 15px 0px rgba(0, 0, 0, 0.35)",
 		fontSize: "1.5rem",
 		margin: "1rem",
 	};
