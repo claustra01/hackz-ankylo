25f0afc2a7ea86ee4d2080e04ce4488344df7671
Semi-koron <6jimoto6@gmail.com>
2024-12-14 22:09:59 +0900

    feat VR player

--- frontend/src/features/game-future/components/GameMap.tsx
+++ frontend/src/features/game-future/components/GameMap.tsx
@@ -4,25 +4,25 @@ import Ground from "./Ground";
 import Target from "./Target";
 
 interface GameMapProps {
-	targetInfos: TargetInfo[];
+  targetInfos: TargetInfo[];
 }
 
 const GameMap = ({ targetInfos }: GameMapProps) => {
-	return (
-		<>
-			<directionalLight position={[0, 1, 1]} intensity={5} />
-			<Ground position={[0, -2, 0]} scale={{ x: 100, z: 100 }} />
-			<Sky
-				distance={450000}
-				sunPosition={[0, 1, 0]}
-				inclination={0}
-				azimuth={0.25}
-			/>
-			{targetInfos.map((targetInfo) => (
-				<Target key={targetInfo.id} targetInfo={targetInfo} />
-			))}
-		</>
-	);
+  return (
+    <>
+      <directionalLight position={[100, 60, 100]} intensity={15} />
+      <Ground position={[0, -2, 0]} scale={{ x: 100, z: 100 }} />
+      <Sky
+        distance={450000}
+        sunPosition={[100, 60, 100]}
+        inclination={0}
+        azimuth={0.25}
+      />
+      {targetInfos.map((targetInfo) => (
+        <Target key={targetInfo.id} targetInfo={targetInfo} />
+      ))}
+    </>
+  );
 };
 
 export default GameMap;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,43 +1,42 @@
 import { OrbitControls } from "@react-three/drei";
-import { Sphere } from "@react-three/drei";
 import { Canvas, useThree } from "@react-three/fiber";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import GameMap from "../features/game-future/components/GameMap";
 import TargetPuttingCollider from "../features/game-future/components/TargetPuttingCollider";
 import useTargetPutting from "../features/game-future/hooks/useTargetPutting";
 import type TargetInfo from "../features/game-future/utils/TargetInfo";
+import { VRPlayer } from "../features/game-future/components/VRPlayer";
 
 interface TargetPuttingManagerProps {
-	targetInfos: TargetInfo[];
-	setTargetInfos: (targetInfos: TargetInfo[]) => void;
+  targetInfos: TargetInfo[];
+  setTargetInfos: (targetInfos: TargetInfo[]) => void;
 }
 
-const TargetPuttingManager = ({
-	targetInfos,
-	setTargetInfos,
-}: TargetPuttingManagerProps) => {
-	const { camera, scene } = useThree();
-	useTargetPutting(camera, scene, targetInfos, setTargetInfos);
-	return null;
-};
-
 function App() {
-	const [targetInfos, setTargetInfos] = useState<TargetInfo[]>([]);
+  const [targetInfos, setTargetInfos] = useState<TargetInfo[]>([]);
+  const arrowRef = useRef<any>(null);
 
-	return (
-		<Canvas style={{ width: "100vw", height: "100vh" }}>
-			<TargetPuttingManager
-				targetInfos={targetInfos}
-				setTargetInfos={setTargetInfos}
-			/>
-			<GameMap targetInfos={targetInfos} />
-			<OrbitControls />
-			<Sphere position={[0, 0, 0]} scale={0.1}>
-				<meshStandardMaterial color="blue" />
-			</Sphere>
-			<TargetPuttingCollider center={[0, 0, 0]} radius={10} />
-		</Canvas>
-	);
+  const TargetPuttingManager = ({
+    targetInfos,
+    setTargetInfos,
+  }: TargetPuttingManagerProps) => {
+    const { camera, scene } = useThree();
+    useTargetPutting(camera, scene, targetInfos, setTargetInfos);
+    return null;
+  };
+
+  return (
+    <Canvas style={{ width: "100vw", height: "100vh" }}>
+      <TargetPuttingManager
+        targetInfos={targetInfos}
+        setTargetInfos={setTargetInfos}
+      />
+      <GameMap targetInfos={targetInfos} />
+      <OrbitControls />
+      <TargetPuttingCollider center={[0, 0, 0]} radius={10} />
+      <VRPlayer arrowRigidBody={arrowRef} />
+    </Canvas>
+  );
 }
 
 export default App;
