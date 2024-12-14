1a8bc3f899b9552223fa8d2a337a5f44a55dbdeb
SakamotoHiroya <ayorih16@icloud.com>
2024-12-14 17:52:30 +0900

    update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,34 @@
 import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
+import { useState } from "react";
 import GameMap from "../features/game-future/components/GameMap";
+import useTargetPutting from "../features/game-future/hooks/useTargetPutting";
+import type TargetInfo from "../features/game-future/utils/TargetInfo";
+
+interface TargetPuttingManagerProps {
+	targetInfos: TargetInfo[];
+	setTargetInfos: (targetInfos: TargetInfo[]) => void;
+}
+
+const TargetPuttingManager = ({
+	targetInfos,
+	setTargetInfos,
+}: TargetPuttingManagerProps) => {
+	const { camera, scene } = useThree();
+	useTargetPutting(camera, scene, targetInfos, setTargetInfos);
+	return null;
+};
 
 function App() {
+	const [targetInfos, setTargetInfos] = useState<TargetInfo[]>([]);
+
 	return (
 		<Canvas style={{ width: "100vw", height: "100vh" }}>
-			<GameMap />
+			<TargetPuttingManager
+				targetInfos={targetInfos}
+				setTargetInfos={setTargetInfos}
+			/>
+			<GameMap targetInfos={targetInfos} />
 			<OrbitControls />
 		</Canvas>
 	);
