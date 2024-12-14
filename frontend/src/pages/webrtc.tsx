f3eae087ea12c21c958392be35d799b518e5f565
claustra01 <shinya430.g@gmail.com>
2024-12-14 17:27:34 +0900

    add: useRTC hooks

--- frontend/src/pages/webrtc.tsx
+++ frontend/src/pages/webrtc.tsx
@@ -1,48 +1,196 @@
-import { useEffect, useRef } from "react";
-import io from "socket.io-client";
+import { useEffect } from "react";
+import { useRTC } from "../hooks/useRTC";
 
-/**
- * communicate to a signaling server
- */
-const ServerTest = () => {
-	const url =
-		import.meta.env.VITE_SIGNALING_SERVER_URL || "http://localhost:8000";
-	const socketRef = useRef<SocketIOClient.Socket>(io.connect(url));
+interface CandidateMessage {
+	type: "candidate";
+	label: number | null;
+	id: string | null;
+	candidate: string;
+}
+
+type TextMessage = "got user media" | "bye";
+
+type Message = TextMessage | RTCSessionDescriptionInit | CandidateMessage;
+
+const Sample5 = () => {
+	const {
+    localVideoRef,
+    remoteVideoRef,
+    peerConnectionRef,
+		localStreamRef,
+    isInitiator,
+    isStarted,
+    isChannelReady,
+		getSocket,
+		getMessageEventTarget,
+		sendMessage,
+		setLocalAndSendMessage,
+		initiatorStart,
+		receiverStart,
+    setIsInitiator,
+    setIsStarted,
+    setIsChannelReady,
+	} = useRTC();
+
 	useEffect(() => {
-		const room = prompt("Enter room name:");
+		console.log(`hostname: ${location.hostname}`);
+		const room = "foo" as string;
 		if (room !== "") {
-			console.log(`Message from client: Asking to join room ${room}`);
-			socketRef.current.emit("create or join", room);
+			console.log(`Asking to join room ${room}`);
+			getSocket().emit("create or join", room);
 		}
-		socketRef.current.on("created", (room: string, clientId: string) => {
+		getSocket().removeAllListeners();
+		getSocket().on("created", (room: string, clientId: string) => {
 			console.log(room, clientId);
+			setIsInitiator(true);
 		});
 
-		socketRef.current.on("full", (room: string) => {
-			console.log(`Message from client: Room ${room} is full :^(`);
+		getSocket().on("full", (room: string) => {
+			console.log(`Room ${room} is full :^(`);
 		});
 
-		socketRef.current.on("ipaddr", (ipaddr: string) => {
-			console.log(`Message from client: Server IP address is ${ipaddr}`);
+		getSocket().on("ipaddr", (ipaddr: string) => {
+			console.log(`Server IP address is ${ipaddr}`);
 		});
 
-		socketRef.current.on("joined", (room: string, clientId: string) => {
+		getSocket().on("join", (room: string) => {
+			console.log(`Another peer made a request to join room ${room}`);
+			console.log(`This peer is the initiator of room ${room}!`);
+			setIsChannelReady(true);
+		});
+
+		getSocket().on("joined", (room: string, clientId: string) => {
 			console.log(room, clientId);
+			setIsChannelReady(true);
 		});
 
-		socketRef.current.on("log", (text: string) => {
+		getSocket().on("log", (text: string) => {
 			console.log(text);
 		});
-	}, []);
+		const byeCallback = () => {
+			console.log("Session terminated.");
+			if (peerConnectionRef.current) peerConnectionRef.current.close();
+			setIsInitiator(true);
+			setIsStarted(false);
+			setIsChannelReady(false);
+		};
+		getMessageEventTarget().addEventListener("bye", byeCallback);
+		const answerCallback = async (e: any) => {
+			const message = e.detail;
+			if (!peerConnectionRef.current) return;
+			peerConnectionRef.current.setRemoteDescription(
+				new RTCSessionDescription(message),
+			);
+		};
+		getMessageEventTarget().addEventListener("answer", answerCallback);
+		getSocket().on("message", async (message: Message) => {
+			if (typeof message === "string") {
+				getMessageEventTarget().dispatchEvent(new Event(message));
+			} else {
+				getMessageEventTarget().dispatchEvent(
+					new CustomEvent(message.type, { detail: message }),
+				);
+			}
+		});
+		(async () => {
+			localStreamRef.current = await navigator.mediaDevices.getUserMedia({
+				video: true,
+			});
+			if (localVideoRef.current) {
+				localVideoRef.current.srcObject = localStreamRef.current;
+				sendMessage("got user media");
+			}
+		})();
 
+		window.addEventListener("beforeunload", () => {
+			sendMessage("bye");
+		});
+
+		return () => {
+			getMessageEventTarget().removeEventListener("bye", byeCallback);
+			getMessageEventTarget().removeEventListener("answer", answerCallback);
+			if (peerConnectionRef.current) peerConnectionRef.current.close();
+			if (localStreamRef.current) localStreamRef.current.getTracks()[0].stop();
+			sendMessage("bye");
+			if (getSocket()) getSocket().close();
+		};
+	}, [sendMessage]);
+
+	useEffect(() => {
+		const callback = () => {
+			console.log("got user media");
+			if (isInitiator) initiatorStart();
+		};
+		getMessageEventTarget().addEventListener("got user media", callback);
+		return () => {
+			getMessageEventTarget().removeEventListener("got user media", callback);
+		};
+	}, [isInitiator, isChannelReady, initiatorStart]);
+
+	useEffect(() => {
+		const callback = async (e: any) => {
+			const message = e.detail;
+			if (!peerConnectionRef.current || !isStarted) return;
+			const candidate = new RTCIceCandidate({
+				sdpMLineIndex: message.label,
+				candidate: message.candidate,
+			});
+			peerConnectionRef.current.addIceCandidate(candidate);
+		};
+		getMessageEventTarget().addEventListener("candidate", callback);
+		return () => {
+			getMessageEventTarget().removeEventListener("candidate", callback);
+		};
+	}, [isStarted]);
+
+	useEffect(() => {
+		const callback = async (e: any) => {
+			const message = e.detail;
+			if (!isInitiator && !isStarted) {
+				await receiverStart();
+			}
+			if (!peerConnectionRef.current) return;
+			peerConnectionRef.current.setRemoteDescription(
+				new RTCSessionDescription(message),
+			);
+			console.log("Sending answer to peer.");
+			const description = await peerConnectionRef.current.createAnswer();
+			setLocalAndSendMessage(description);
+		};
+		getMessageEventTarget().addEventListener("offer", callback);
+		return () => {
+			getMessageEventTarget().removeEventListener("offer", callback);
+		};
+	}, [
+		isInitiator,
+		isStarted,
+		isChannelReady,
+		receiverStart,
+		setLocalAndSendMessage,
+	]);
+
+	console.log("render end");
 	return (
 		<div>
-			<h2>Sample 4</h2>
-			<p>
-				communicate to a signaling server. run <b>yarn start:server</b>
-			</p>
+			<h2>Sample 5</h2>
+			<p>Signaling and video Peer Connection</p>
+			<p>isStarted: {String(isStarted)},</p>
+			<p>isChannelReady: {String(isChannelReady)}</p>
+			<p>isInitiator: {String(isInitiator)}</p>
+			<video
+				ref={localVideoRef}
+				style={{ width: "320px", maxWidth: "100%" }}
+				autoPlay
+				playsInline
+			/>
+			<video
+				ref={remoteVideoRef}
+				style={{ width: "320px", maxWidth: "100%" }}
+				autoPlay
+				playsInline
+			/>
 		</div>
 	);
 };
 
-export default ServerTest;
+export default Sample5;
