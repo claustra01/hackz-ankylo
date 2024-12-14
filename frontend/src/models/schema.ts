8c344fdaea0edfdcd7d9cc2f1685cec754d8c4ff
claustra01 <shinya430.g@gmail.com>
2024-12-14 22:31:44 +0900

    fix: lint

--- frontend/src/models/schema.ts
+++ frontend/src/models/schema.ts
@@ -1,12 +1,12 @@
-import { Vector3 } from "@react-three/fiber"
+import type { Vector3 } from "@react-three/fiber";
 
 export type SetTargetMessage = {
-  type: "set-target"
-  pos: Vector3
-}
+	type: "set-target";
+	pos: Vector3;
+};
 
 export type ShootArrowMessage = {
-  type: "shoot-arrow"
-  pos: Vector3
-  dir: Vector3
-}
+	type: "shoot-arrow";
+	pos: Vector3;
+	dir: Vector3;
+};
