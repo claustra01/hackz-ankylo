384fd2ce1f9051da7a97481d0f053d319f02d39d
Semi-koron <6jimoto6@gmail.com>
2024-12-14 23:48:13 +0900

    fix lint error

--- frontend/src/features/game-future/components/VRPlayer.tsx
+++ frontend/src/features/game-future/components/VRPlayer.tsx
@@ -1,93 +1,93 @@
-import {
-  XR,
-  createXRStore,
-  useXRInputSourceState,
-  XROrigin,
-} from "@react-three/xr";
-import { Physics, RigidBody } from "@react-three/rapier";
 import { useFrame } from "@react-three/fiber";
-import { useState, useRef } from "react";
-import { Mesh, Vector3 } from "three";
-import { Group } from "three";
+import { Physics, RigidBody } from "@react-three/rapier";
+import {
+	XR,
+	XROrigin,
+	createXRStore,
+	useXRInputSourceState,
+} from "@react-three/xr";
+import { useRef, useState } from "react";
+import { type Mesh, Vector3 } from "three";
+import type { Group } from "three";
 import { useThrowArrow } from "../hooks/useThrowArrow";
 const store = createXRStore();
 
 interface VRPlayerProps {
-  arrowRigidBody: any;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	arrowRigidBody: any;
 }
 
 export const VRPlayer = ({ arrowRigidBody }: VRPlayerProps) => {
-  const [rightGrab, setRightGrab] = useState(false);
-  const [isVR, setIsVR] = useState(false);
-  const leftMeshRef = useRef<Mesh>(null);
-  const rightMeshRef = useRef<Mesh>(null);
-  const Locomotion = () => {
-    const leftController = useXRInputSourceState("controller", "left");
-    const rightController = useXRInputSourceState("controller", "right");
-    const ref = useRef<Group>(null);
-    useFrame(() => {
-      if (!ref.current || !leftController || !rightController) return;
+	const [rightGrab, setRightGrab] = useState(false);
+	const leftMeshRef = useRef<Mesh>(null);
+	const rightMeshRef = useRef<Mesh>(null);
+	const Locomotion = () => {
+		const leftController = useXRInputSourceState("controller", "left");
+		const rightController = useXRInputSourceState("controller", "right");
+		const ref = useRef<Group>(null);
+		useFrame(() => {
+			if (!ref.current || !leftController || !rightController) return;
 
-      const leftSqueezeState = leftController.gamepad["xr-standard-squeeze"];
-      const rightSqueezeState = rightController.gamepad["xr-standard-squeeze"];
-      if (!leftSqueezeState || !rightSqueezeState) return;
+			const leftSqueezeState = leftController.gamepad["xr-standard-squeeze"];
+			const rightSqueezeState = rightController.gamepad["xr-standard-squeeze"];
+			if (!leftSqueezeState || !rightSqueezeState) return;
 
-      if (leftSqueezeState.state === "pressed") {
-        const leftControllerPos = new Vector3();
-        leftController.object?.getWorldPosition(leftControllerPos);
-        if (leftMeshRef.current) {
-          leftMeshRef.current.position.copy(leftControllerPos);
-        }
-      }
-      if (rightSqueezeState.state === "pressed") {
-        const rightControllerPos = new Vector3();
-        rightController.object?.getWorldPosition(rightControllerPos);
-        if (rightMeshRef.current) {
-          rightMeshRef.current.position.copy(rightControllerPos);
-        }
-        setRightGrab(true);
-      } else {
-        if (rightGrab && leftMeshRef.current) {
-          throwArrow();
-          setRightGrab(false);
-        }
-      }
-    });
-    return <XROrigin ref={ref} />;
-  };
+			if (leftSqueezeState.state === "pressed") {
+				const leftControllerPos = new Vector3();
+				leftController.object?.getWorldPosition(leftControllerPos);
+				if (leftMeshRef.current) {
+					leftMeshRef.current.position.copy(leftControllerPos);
+				}
+			}
+			if (rightSqueezeState.state === "pressed") {
+				const rightControllerPos = new Vector3();
+				rightController.object?.getWorldPosition(rightControllerPos);
+				if (rightMeshRef.current) {
+					rightMeshRef.current.position.copy(rightControllerPos);
+				}
+				setRightGrab(true);
+			} else {
+				if (rightGrab && leftMeshRef.current) {
+					throwArrow();
+					setRightGrab(false);
+				}
+			}
+		});
+		return <XROrigin ref={ref} />;
+	};
 
-  const throwArrow = () => {
-    const leftController = useXRInputSourceState("controller", "left");
-    if (!leftController) return;
-    const leftControllerPos = new Vector3();
-    leftController.object?.getWorldPosition(leftControllerPos);
-    const rightControllerPos = new Vector3();
-    rightMeshRef.current?.getWorldPosition(rightControllerPos);
-    const rotateVector3 = rightControllerPos.sub(leftControllerPos);
-    useThrowArrow(leftControllerPos, rotateVector3, arrowRigidBody);
-  };
+	const throwArrow = () => {
+		const leftController = useXRInputSourceState("controller", "left");
+		if (!leftController) return;
+		const leftControllerPos = new Vector3();
+		leftController.object?.getWorldPosition(leftControllerPos);
+		const rightControllerPos = new Vector3();
+		rightMeshRef.current?.getWorldPosition(rightControllerPos);
+		const rotateVector3 = rightControllerPos.sub(leftControllerPos);
+		useThrowArrow(leftControllerPos, rotateVector3, arrowRigidBody);
+	};
 
-  return (
-    <XR store={store}>
-      <Physics gravity={[0, -9.81, 0]}>
-        <mesh ref={leftMeshRef} scale={[0.01, 0.01, 0.01]}>
-          <boxGeometry />
-          <meshStandardMaterial color="yellow" metalness={0} roughness={0.2} />
-        </mesh>
+	return (
+		<XR store={store}>
+			<Physics gravity={[0, -9.81, 0]}>
+				<mesh ref={leftMeshRef} scale={[0.01, 0.01, 0.01]}>
+					<boxGeometry />
+					<meshStandardMaterial color="yellow" metalness={0} roughness={0.2} />
+				</mesh>
 
-        <mesh ref={rightMeshRef} scale={[0.01, 0.01, 0.01]}>
-          <boxGeometry />
-          <meshStandardMaterial color="cyan" metalness={0} roughness={0.2} />
-        </mesh>
+				<mesh ref={rightMeshRef} scale={[0.01, 0.01, 0.01]}>
+					<boxGeometry />
+					<meshStandardMaterial color="cyan" metalness={0} roughness={0.2} />
+				</mesh>
 
-        <RigidBody ref={arrowRigidBody}>
-          <mesh scale={[0.25, 0.25, 0.25]} position={[0, -3, 0]}>
-            <sphereGeometry />
-            <meshStandardMaterial color="cyan" metalness={0} roughness={0.2} />
-          </mesh>
-        </RigidBody>
-        <Locomotion />
-      </Physics>
-    </XR>
-  );
+				<RigidBody ref={arrowRigidBody}>
+					<mesh scale={[0.25, 0.25, 0.25]} position={[0, -3, 0]}>
+						<sphereGeometry />
+						<meshStandardMaterial color="cyan" metalness={0} roughness={0.2} />
+					</mesh>
+				</RigidBody>
+				<Locomotion />
+			</Physics>
+		</XR>
+	);
 };
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,42 +1,43 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas, useThree } from "@react-three/fiber";
-import { useState, useRef } from "react";
+import { useRef, useState } from "react";
 import GameMap from "../features/game-future/components/GameMap";
 import TargetPuttingCollider from "../features/game-future/components/TargetPuttingCollider";
+import { VRPlayer } from "../features/game-future/components/VRPlayer";
 import useTargetPutting from "../features/game-future/hooks/useTargetPutting";
 import type TargetInfo from "../features/game-future/utils/TargetInfo";
-import { VRPlayer } from "../features/game-future/components/VRPlayer";
 
 interface TargetPuttingManagerProps {
-  targetInfos: TargetInfo[];
-  setTargetInfos: (targetInfos: TargetInfo[]) => void;
+	targetInfos: TargetInfo[];
+	setTargetInfos: (targetInfos: TargetInfo[]) => void;
 }
 
 function App() {
-  const [targetInfos, setTargetInfos] = useState<TargetInfo[]>([]);
-  const arrowRef = useRef<any>(null);
+	const [targetInfos, setTargetInfos] = useState<TargetInfo[]>([]);
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	const arrowRef = useRef<any>(null);
 
-  const TargetPuttingManager = ({
-    targetInfos,
-    setTargetInfos,
-  }: TargetPuttingManagerProps) => {
-    const { camera, scene } = useThree();
-    useTargetPutting(camera, scene, targetInfos, setTargetInfos);
-    return null;
-  };
+	const TargetPuttingManager = ({
+		targetInfos,
+		setTargetInfos,
+	}: TargetPuttingManagerProps) => {
+		const { camera, scene } = useThree();
+		useTargetPutting(camera, scene, targetInfos, setTargetInfos);
+		return null;
+	};
 
-  return (
-    <Canvas style={{ width: "100vw", height: "100vh" }}>
-      <TargetPuttingManager
-        targetInfos={targetInfos}
-        setTargetInfos={setTargetInfos}
-      />
-      <GameMap targetInfos={targetInfos} />
-      <OrbitControls />
-      <TargetPuttingCollider center={[0, 0, 0]} radius={10} />
-      <VRPlayer arrowRigidBody={arrowRef} />
-    </Canvas>
-  );
+	return (
+		<Canvas style={{ width: "100vw", height: "100vh" }}>
+			<TargetPuttingManager
+				targetInfos={targetInfos}
+				setTargetInfos={setTargetInfos}
+			/>
+			<GameMap targetInfos={targetInfos} />
+			<OrbitControls />
+			<TargetPuttingCollider center={[0, 0, 0]} radius={10} />
+			<VRPlayer arrowRigidBody={arrowRef} />
+		</Canvas>
+	);
 }
 
 export default App;
