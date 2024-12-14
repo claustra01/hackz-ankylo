6b74d9ab3d2112042dbe19490f239a98d8727054
claustra01 <shinya430.g@gmail.com>
2024-12-14 12:38:30 +0900

    chore: sort import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { Routes } from '@generouted/react-router'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(<Routes />)
