900ff4e50b93303e3b44ce5384185d873583990c
Semi-koron <6jimoto6@gmail.com>
2024-12-15 12:53:24 +0900

    fix lint error

--- frontend/src/components/Arrow.tsx
+++ frontend/src/components/Arrow.tsx
@@ -1,17 +1,17 @@
 import { RigidBody } from "@react-three/rapier";
 
 interface ArrowProps {
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  arrowRigidBody: any;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	arrowRigidBody: any;
 }
 
 export const Arrow = ({ arrowRigidBody }: ArrowProps) => {
-  return (
-    <RigidBody ref={arrowRigidBody}>
-      <mesh position={[0, 0, 0]}>
-        <cylinderGeometry args={[0.1, 0.1, 1, 32]} />
-        <meshStandardMaterial color="cyan" metalness={0} roughness={0.2} />
-      </mesh>
-    </RigidBody>
-  );
+	return (
+		<RigidBody ref={arrowRigidBody}>
+			<mesh position={[0, 0, 0]}>
+				<cylinderGeometry args={[0.1, 0.1, 1, 32]} />
+				<meshStandardMaterial color="cyan" metalness={0} roughness={0.2} />
+			</mesh>
+		</RigidBody>
+	);
 };
--- frontend/src/utils/throwArrow.ts
+++ frontend/src/utils/throwArrow.ts
@@ -2,22 +2,22 @@ import type * as THREE from "three";
 import type { Vector3 } from "three"; // threeから直接インポート
 
 export const throwArrow = (
-  startVector: Vector3,
-  rotateVector3: Vector3,
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  arrow: any,
-  arrowRef: React.RefObject<THREE.Mesh>
+	startVector: Vector3,
+	rotateVector3: Vector3,
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	arrow: any,
+	arrowRef: React.RefObject<THREE.Mesh>,
 ) => {
-  const rigidBody = arrow.current;
-  if (rigidBody) {
-    rigidBody.setTranslation(startVector, true); // 初期位置に戻す
-    rigidBody.setLinvel({ x: 0, y: 0, z: 0 }, true); // 線形速度をリセット
-    rigidBody.setAngvel({ x: 0, y: 0, z: 0 }, true); // 角速度をリセット
-  }
-  arrow.current.applyImpulse(rotateVector3, true);
-  if (arrowRef.current) {
-    arrowRef.current.lookAt(rotateVector3);
-    //z軸を中心に90度回転
-    arrowRef.current.rotateX(Math.PI / 2);
-  }
+	const rigidBody = arrow.current;
+	if (rigidBody) {
+		rigidBody.setTranslation(startVector, true); // 初期位置に戻す
+		rigidBody.setLinvel({ x: 0, y: 0, z: 0 }, true); // 線形速度をリセット
+		rigidBody.setAngvel({ x: 0, y: 0, z: 0 }, true); // 角速度をリセット
+	}
+	arrow.current.applyImpulse(rotateVector3, true);
+	if (arrowRef.current) {
+		arrowRef.current.lookAt(rotateVector3);
+		//z軸を中心に90度回転
+		arrowRef.current.rotateX(Math.PI / 2);
+	}
 };
