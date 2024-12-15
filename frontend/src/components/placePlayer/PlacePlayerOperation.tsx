00aa995fcbef540680013785abecbfebc6367446
SakamotoHiroya <ayorih16@icloud.com>
2024-12-15 11:23:03 +0900

    add name to arrow

--- frontend/src/components/placePlayer/PlacePlayerOperation.tsx
+++ frontend/src/components/placePlayer/PlacePlayerOperation.tsx
@@ -123,7 +123,7 @@ const PlacePlayerOperation = () => {
 					onShoot={handleShoot}
 				/>
 			))}
-			<RigidBody ref={arrowRigitRef}>
+			<RigidBody ref={arrowRigitRef} name="arrow">
 				<mesh scale={[0.25, 0.25, 0.25]} position={[0, 0, 0]}>
 					<sphereGeometry />
 					<meshStandardMaterial color="cyan" metalness={0} roughness={0.2} />
